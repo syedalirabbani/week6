482f72409cdc90964378fa7fba56d43339efa571
Syed Ali Rabbani <77966033+syedalirabbani@users.noreply.github.com>
2021-03-09 16:50:11 +0800

    Create factorialize.js

---
+++ factorialize.js
@@ -0,0 +1,14 @@
+//--------------
+//factorialize a number
+//-------------
+
+function factorialize(num){
+  if ( num == 0){
+    return 1;
+  }
+  else{
+    return num*factorialize(num-1);
+  }
+}
+
+console.log(factorialize(5));
